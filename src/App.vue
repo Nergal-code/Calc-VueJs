a96d7395b944acf32ca505a74a795f0906ca2145
Caio Lima <53834100+Nergal-code@users.noreply.github.com>
2023-12-10 15:49:08 -0300

    Calculadora VueJS
    Calculadora VueJS, teste project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+    import calculadora from './components/calculadora.vue';
+    
+    export default {
+        components: {
+        calculadora
+        }
+    };
+</script>
+
+<template>
+    <div id="app">
+        <h1>Calculadora Vue</h1>
+        <calculadora />
+    </div>
+</template>
+
+<style>
+    #app {
+        text-align: center;
+    }
+</style>
+  
---
+++ src/components/calculadora.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="calculadora">
+        <input v-model.numero="numero1" type="number" placeholder="Número 1" @input="calcular">
+        <select v-model="operacao" @change="calcular">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input v-model.numero="numero2" type="number" placeholder="Número 2" @input="calcular">
+        <div class="resultado">Resultado: {{ Resultado }}</div>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+        return {
+            numero1: 0,
+            numero2: 0,
+            operacao: '+',
+            Resultado: 0
+        };
+    },
+    methods: {
+      calcular() {
+        switch (this.operacao) {
+            case '+':
+                this.Resultado = this.numero1 + this.numero2;
+                break;
+            case '-':
+                this.Resultado = this.numero1 - this.numero2;
+                break;
+            case '*':
+                this.Resultado = this.numero1 * this.numero2;
+                break;
+            case '/':
+                this.Resultado = this.numero1 / this.numero2;
+                break;
+            default:
+                this.Resultado = 0;
+        }
+      }
+    }
+  };
+</script>
+<style scoped>
+    .calculadora {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 50px;
+    }
+    
+    input[type="number"], select {
+        margin-bottom: 10px;
+    }
+    
+    .resultado {
+        margin-top: 10px;
+        font-weight: bold;
+    }
+</style>  
